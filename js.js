4e1072bf7264396633b022d434723c7b65e08cde
lalala2223 <mrrobo330@gmail.com>
2023-12-17 17:02:27 +0200

    dsfdf

---
+++ js.js
@@ -0,0 +1,17 @@
+{/* <div class="product1">
+<h1>BMW M5F90</h1>
+<img src="https://vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg">
+<p>Price:</p>
+<p>Description:</p>
+<a>seller profile</a><br>
+<button>Buy</button>
+</div> */}
+let productsArray =[]
+let xhr = new HMLHttpRequest[]
+let url ='https://my-json-server.typicode.com/lalala2223/asasa';
+zhr.open('GET',url+"products")
+xhr.responseType="json";
+xhr.onload=function(){
+    let products =xhr.responseType;
+    console.log("products")
+}
