a4767dc492f54d2cdca9dea33114e8587b66bb99
lalala2223 <mrrobo330@gmail.com>
2023-12-17 17:46:31 +0200

    sa

--- js.js
+++ js.js
@@ -14,6 +14,19 @@ xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
 xhr.onload = function(){
     let products = xhr.response;
-    console.log(products[0].name)
+    console.log(products.length)
+    for(let i=0;i<products.length;i++){
+        let html=`
+        <div class="product1">
+            <h1>${products[i].name}</h1>
+            <img src="${products[i].photo_url}">
+            <p>Price:${products[i].price}$</p>
+            <p>Description ${products[i].description}</p>
+            <a>seller profile</a><br>
+            <button>Buy</button>
+        </div>
+        `
+        $("#products-grid").append(html);
+    }
 }
 xhr.send()
