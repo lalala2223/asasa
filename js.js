c98ef1c633ec8d7991337f80a43b2b784a7f3553
lalala2223 <mrrobo330@gmail.com>
2023-12-17 17:14:44 +0200

    sdfds

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
 {/* <div class="product1">
-<h1>BMW M5F90</h1>
-<img src="https://vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg">
-<p>Price:</p>
-<p>Description:</p>
-<a>seller profile</a><br>
-<button>Buy</button>
+    <h1>BMW M5F90</h1>
+    <img src="https://vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg">
+    <p>Price:</p>
+    <p>Description:</p>
+    <a>seller profile</a><br>
+    <button>Buy</button>
 </div> */}
-let productsArray =[]
-let xhr = new HMLHttpRequest()
-let url ='https://my-json-server.typicode.com/lalala2223/asasa';
-zhr.open('GET',url+"/products")
-xhr.responseType="json";
-xhr.onload=function(){
-    let products =xhr.responseType;
-    console.log(products[0].id)
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/Alekskol25/chupapapi123';
+xhr.open('GET', url + '/products');
+xhr.responseType = 'json';
+xhr.onload = function(){
+    let products = xhr.response;
+    console.log(products[0].name)
 }
-xhr.send();
+xhr.send()
