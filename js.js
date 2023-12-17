ef64f2deff7120f54a6601c4af04a075d3d218d0
lalala2223 <mrrobo330@gmail.com>
2023-12-17 17:15:28 +0200

    fsdf

--- js.js
+++ js.js
@@ -1,14 +1,14 @@
-{/* <div class="product1">
+/* <div class="product1">
     <h1>BMW M5F90</h1>
     <img src="https://vrcpitbull.com/wp-content/uploads/2011/02/VAMPVAMP%E2%80%A2.jpg">
     <p>Price:</p>
     <p>Description:</p>
     <a>seller profile</a><br>
     <button>Buy</button>
-</div> */}
+</div> */
 let productsArray = [];
 let xhr = new XMLHttpRequest();
-let url = 'https://my-json-server.typicode.com/Alekskol25/chupapapi123';
+let url = 'https://my-json-server.typicode.com/lalala2223/asasa';
 xhr.open('GET', url + '/products');
 xhr.responseType = 'json';
 xhr.onload = function(){
